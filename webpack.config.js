2ff0b38072085acff2a42dac97b9e466790c7deb
Takumi Endo <tq-ed@v7.com>
2017-12-20 01:10:03 +0900

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,23 @@ module.exports = {
   entry: "./src/main.js",
   output: {
     filename: "build/bundle.js"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [ 'env', { 'modules': false } ]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map'
 }
